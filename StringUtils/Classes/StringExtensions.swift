33d56d8ab83f78e4dca69fb4c6e8691328d6234e
Jmeggesto <jackie.meggesto@gmail.com>
2016-05-24 17:08:52 -0400

    more extensive emoji testing

--- StringUtils/Classes/StringExtensions.swift
+++ StringUtils/Classes/StringExtensions.swift
@@ -183,7 +183,9 @@ public extension String {
         for scalar in unicodeScalars {
             switch scalar.value {
             case 0x1F600...0x1F64F, // Emoticons
-            0x1F300...0x1F5FF, // Misc Symbols and Pictographs
+            0x1F300...0x1F5FF,
+            0x1F900...0x1F9FF,
+            0x1F600...0x1F64F, // Misc Symbols and Pictographs
             0x1F680...0x1F6FF, // Transport and Map
             0x2600...0x26FF,   // Misc symbols
             0x2700...0x27BF,   // Dingbats
